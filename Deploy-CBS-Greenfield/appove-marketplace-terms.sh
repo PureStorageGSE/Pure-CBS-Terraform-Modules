7006e09f5a0473562b28c888b14283a2df3e294b
psg18dhc <116092796+psg18dhc@users.noreply.github.com>
2024-02-14 17:45:40 +0000

    add marketplace approve script

---
+++ Deploy-CBS-Greenfield/appove-marketplace-terms.sh
@@ -0,0 +1,3 @@
+#terraform import "azurerm_marketplace_agreement.plan"  "/subscriptions/f4c99595-b139-4086-a00c-30da54ce80e9/providers/Microsoft.MarketplaceOrdering/agreements/purestoragemarketplaceadmin/offers/pure_storage_cloud_block_store_deployment/plans/cbs_azure_6_4_10"
+terraform import "azurerm_marketplace_agreement.plan"  "/subscriptions/f4c99595-b139-4086-a00c-30da54ce80e9/providers/Microsoft.MarketplaceOrdering/agreements/purestoragemarketplaceadmin/offers/pure_storage_cloud_block_store_deployment/plans/cbs_azure_6_6_3"
+az vm image terms accept --offer pure_storage_cloud_block_store_deployment --plan cbs_azure_6_6_3 --publisher purestoragemarketplaceadmin
