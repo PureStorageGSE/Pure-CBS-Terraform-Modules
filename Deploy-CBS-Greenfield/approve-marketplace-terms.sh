1699973ceb7e6a9c2ae918c43ad879f7b35011cb
psg18dhc <116092796+psg18dhc@users.noreply.github.com>
2024-02-29 15:41:22 +0000

    revise approve marketplace and import for 6.6.4

--- Deploy-CBS-Greenfield/approve-marketplace-terms.sh
+++ Deploy-CBS-Greenfield/approve-marketplace-terms.sh
@@ -1,3 +1,7 @@
 #terraform import "azurerm_marketplace_agreement.plan"  "/subscriptions/f4c99595-b139-4086-a00c-30da54ce80e9/providers/Microsoft.MarketplaceOrdering/agreements/purestoragemarketplaceadmin/offers/pure_storage_cloud_block_store_deployment/plans/cbs_azure_6_4_10"
+#marketplace approval
 terraform import "azurerm_marketplace_agreement.plan"  "/subscriptions/f4c99595-b139-4086-a00c-30da54ce80e9/providers/Microsoft.MarketplaceOrdering/agreements/purestoragemarketplaceadmin/offers/pure_storage_cloud_block_store_deployment/plans/cbs_azure_6_6_3"
+terraform import "azurerm_marketplace_agreement.plan"  "/subscriptions/f4c99595-b139-4086-a00c-30da54ce80e9/providers/Microsoft.MarketplaceOrdering/agreements/purestoragemarketplaceadmin/offers/pure_storage_cloud_block_store_deployment/plans/cbs_azure_6_6_3"
+# depennds on which release - copy and apply accordingly.
 az vm image terms accept --offer pure_storage_cloud_block_store_deployment --plan cbs_azure_6_6_3 --publisher purestoragemarketplaceadmin
+az vm image terms accept --offer pure_storage_cloud_block_store_deployment --plan cbs_azure_6_6_4 --publisher purestoragemarketplaceadmin
