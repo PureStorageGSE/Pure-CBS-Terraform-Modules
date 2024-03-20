867a3b45fe4e7158506d126ee4c9c6adfbddc70a
psg18dhc <116092796+psg18dhc@users.noreply.github.com>
2024-03-20 12:36:53 +0000

    changes to readme

--- Deploy-CBS-Existing-Infra/README.md
+++ Deploy-CBS-Existing-Infra/README.md
@@ -1,4 +1,30 @@
 # Deploy CBS Into an Existing Azure Subscription with a specific resource group
 
-# Introduction
-## USE this to deploy into an existing RG/VNet designated for CBS
+## What does this code do
+
+This terraform code will deploy a CBS Array into an existing Azure Resource Group which contains a Vnet and four subnets.
+
+
+### If you want to route between your VNets from On Premise or another Azure subscription
+
+You may be required to setup the Vnet peering between your managment and CBS vnet by uncommenting the Module CBS-VNet-Peering code in `main.tf` file on lines `144-152`, unless you have already done this.
+
+The Modules/CBS-VNet-Peering  `main.tf` code has options to activate this which are commented out and follow the four traffic routing options in the Peering section in the Azure UI
+
+Vis.
+
+
+![](assets/20240320_122712_CBS-Generic-Two-RG-Deployment.png)
+
+
+## How to Use
+
+1. Deploy an Azure VM into the same subscription where you are planning to deploy CBS
+2. Identify and obtain the names of the following resources.
+
+- a resource group that you have created for CBS
+- a VNET and four subnets that you want to use for CBS. Namely, system, management, iscsi and replication
+
+This code will deploy the dependencies and CBS Array into the VNet which you choose along with the VNet peering between the Vnets.
+
+We used a /21 for each separate vnet
--- Modules/CBS-VNet-Peering/main.tf
+++ Modules/CBS-VNet-Peering/main.tf
@@ -1,20 +1,32 @@
 
-// setup network peering for access from tf vnet to cbs vnet
+// setup network peering for access from your CBS deployment vnet to cbs vnet
  data "azurerm_virtual_network" "tf_vnet" {
-   name                = var.azure_virtualnetwork_peer_name
-   resource_group_name = var.azure_virtualnetwork_peer_rg
+   name                     = var.azure_virtualnetwork_peer_name
+   resource_group_name      = var.azure_virtualnetwork_peer_rg
 }
 
+// Configure two way peering from management vnet to cbs vnet
 resource "azurerm_virtual_network_peering" "tf_cbs" {
-  name                = "To-CBS-VNET"
-  resource_group_name = var.azure_virtualnetwork_peer_rg
+  name                      = "To-CBS-VNET"
+  resource_group_name       = var.azure_virtualnetwork_peer_rg
   virtual_network_name      = data.azurerm_virtual_network.tf_vnet.name
   remote_virtual_network_id = var.cbs_vnet_id
+  #  configure routing between vnets
+  # allow_forwarded_traffic = true
+  # allow_virtual_network_access = true
+  # allow_gateway_transit = true
 }
 
+// Configure two way peering back from CBS Net to management Vnet
 resource "azurerm_virtual_network_peering" "cbs_tf" {
   name                      = "From-CBS-VNET"
-  resource_group_name = var.resource_group_name
+  resource_group_name       = var.resource_group_name
   virtual_network_name      = var.cbs_vnet_name
   remote_virtual_network_id = data.azurerm_virtual_network.tf_vnet.id
+  # configure routing between vnets
+  allow_forwarded_traffic = true
+  allow_virtual_network_access = true
+  allow_gateway_transit = true
+  use_remote_gateways = true
+
 }
